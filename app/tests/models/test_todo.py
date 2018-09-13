0f6e66ae3ae2d2160481d70d09270f0ff4add650
Mohammad Alrefai <malrefai@nyit.edu>
2018-09-13 14:15:03 +0300

    Add test suite for the todo model

---
+++ app/tests/models/test_todo.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.db import models
+
+from app.tests.factories.todos import TodoFactory
+
+
+class TodoModelTest(TestCase):
+    """
+    This class defines the test suite for the todo model.
+    """
+
+    todo = None
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        TodoModelTest.todo = TodoFactory.build()
+
+    def test_title_field(self):
+        field = TodoModelTest.todo._meta.get_field('title')
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 100)
+        self.assertFalse(field.blank)
+
+    def test_owner_field(self):
+        field = TodoModelTest.todo._meta.get_field('owner')
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 100)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+    def test_created_field(self):
+        field = TodoModelTest.todo._meta.get_field('created')
+        self.assertIsInstance(field, models.DateTimeField)
+        self.assertTrue(field.auto_now_add)
+        self.assertFalse(field.blank)
+
+    def test_modified_field(self):
+        field = TodoModelTest.todo._meta.get_field('modified')
+        self.assertIsInstance(field, models.DateTimeField)
+        self.assertTrue(field.auto_now)
+        self.assertFalse(field.blank)
