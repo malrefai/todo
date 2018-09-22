449f7a705553521ef9d4e945851eb53d317f3ef9
Mohammad Alrefai <malrefai@nyit.edu>
2018-09-22 11:34:09 +0300

    Add jsonref and ability to support $ref

--- app/tests/support/assertions.py
+++ app/tests/support/assertions.py
@@ -1,6 +1,6 @@
 from os.path import join, dirname
-from json import loads
 from jsonschema import validate
+from jsonref import loads
 
 
 def asert_valid_schema(data, schema_file):
@@ -15,8 +15,11 @@ def _load_json_schema(filename):
     """
     Loads the given schema file.
     """
-    relative_path = join('schema', filename)
+    relative_path = join("schemas", filename)
     absolute_path = join(dirname(dirname(__file__)), relative_path)
 
+    base_path = dirname(absolute_path)
+    base_uri = f"file://{base_path}/"
+
     with open(absolute_path) as schema_file:
-        return loads(schema_file.read())
+        return loads(schema_file.read(), base_uri=base_uri, jsonschema=True)
